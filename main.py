66c5d886e79043e06398812066546721c3d168ce
mopheshi <ndachimya@gmail.com>
2023-03-30 01:17:16 +0100

    Detect faces in an image and return the blurred faces...

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+import cv2
+from cvzone.FaceDetectionModule import FaceDetector
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)  # width
+cap.set(4, 480)  # height
 
+# Minimum Detection Confidence Threshold = 0.75 for optimal facial detection
+detector = FaceDetector(minDetectionCon=0.75)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+while True:
+    success, image = cap.read()
+    # Find faces in an image and return the bounding box information and draw around the detected face if draw=True
+    image, boundingBoxes = detector.findFaces(image, draw=False)
 
+    # If bounding box(es) is/are not empty/null
+    if boundingBoxes:
+        # Give different names for each available bounding box with 'i' as the unique identifier
+        for i, boundingBox in enumerate(boundingBoxes):
+            x, y, w, h = boundingBox["bbox"]
+            # To avoid errors when an image is very close to the camera and gets out of bounds
+            if x < 0: x = 0
+            if y < 0: y = 0
+            # Cropping image with: 'y' as starting height and 'y + h' as ending height... Alternatively, 'x' is the
+            # same for width
+            croppedFace = image[y:y + h, x:x + w]
+            # Blur cropped face(s), (35, 35) must be odd numbers, and it's the degree of blur needed on the face
+            blurredFace = cv2.blur(croppedFace, (35, 35))
+            image[y:y + h, x:x + w] = blurredFace  # Replace cropped face with the blurred to hide the detected face
+            # Display detected cropped face(s)
+            # cv2.imshow(f"Cropped face {i}", image)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    cv2.imshow("Image", image)
+    cv2.waitKey(1)  # 1 millisecond delay
